7749a8c4601bd27421340cc3273a2681bb380a5d
Zhogolev <konszhog@gmail.com>
2018-07-09 17:20:13 +0700

    unused

--- db.js
+++
@@ -1,2 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:admin364@ds125211.mlab.com:25211/coc-coc-celebrities');
--- user/User.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');  
-var UserSchema = new mongoose.Schema({  
-  name: String,
-  email: {type: String, index: {unique: true, dropDups: true}},
-  password: String
-});
-mongoose.model('User', UserSchema);
-
-module.exports = mongoose.model('User');
